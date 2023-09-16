2b05c304a1bad9243d6c96de0c1478dfb37d606c
elbug-py <vbromero@miuandes.cl>
2023-09-15 21:31:42 -0300

    Now you can actually search in the searchbar of the map also reset to the current location and the styling is fiexd AND ITS IMPLEMENTED ON REACT

--- src/components/MapPage/MapPage.js
+++ src/components/MapPage/MapPage.js
@@ -1,66 +1,93 @@
-// MapPage
-
-import React, { Component,useState,useEffect } from 'react';
-import {compose, withProps } from "recompose"
-import { useQuery } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
-import {Box, Button, Container, CssBaseline, Paper, Stack, styled} from "@mui/material";
-import TextField from "@mui/material/TextField";
-import Grid2 from "@mui/material/Unstable_Grid2";
-import CircularProgress from "@mui/material/CircularProgress";
-import SpinnerOfDoom from "../HomePage/SpinnerOfDoom";
+import React, { useState, useEffect } from 'react';
+import { compose, withProps } from 'recompose';
 import {
-    withScriptjs,
-    withGoogleMap,
-    GoogleMap,
-    Marker
-  } from "react-google-maps";
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Paper,
+  Grid,
+  styled,
+  TextField,
+} from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
+import SpinnerOfDoom from '../HomePage/SpinnerOfDoom';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from 'react-google-maps';
 
+const api_key = process.env.REACT_APP_GAPI_KEY;
 
+const SearchBar = ({ onSearch, onSearchCurrentLocation }) => {
+  const [search, setSearch] = useState('');
 
-const api_key = process.env.REACT_APP_GAPI_KEY
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
 
+  const handleSearchClick = () => {
+    onSearch(search);
+  };
 
-
-
-
-
+  return (
+    <Grid container spacing={2} alignItems="center">
+      <Grid item xs={8}>
+        <TextField
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          fullWidth
+          value={search}
+          onChange={handleSearchChange}
+        />
+      </Grid>
+      <Grid item xs={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleSearchClick}
+          fullWidth
+        >
+          Search
+        </Button>
+      </Grid>
+      <Grid item xs={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onSearchCurrentLocation}
+          fullWidth
+        >
+          Current Location
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
 
 function MapPage(props) {
-    const [latitude, setLatitude] = useState(null);
-    const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
 
-    useEffect(() => {
-      navigator.geolocation.getCurrentPosition(
-          (position) => {
-              setLatitude(position.coords.latitude);
-              setLongitude(position.coords.longitude);
-              console.log(position.coords.latitude);
-              console.log(position.coords.longitude);
-          },
-          (error) => {
-              console.log(error);
-          }
-      );
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }, []);
 
-    const ResetLocation = () => {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-            setLatitude(position.coords.latitude);
-            setLongitude(position.coords.longitude);
-        },
-        (error) => {
-            console.log(error);
-        }
-    );
-    }
-
-    const SearchLocation = (event) => {
-      event.preventDefault();
-      const data = document.getElementById("outlined-basic");
-      console.log(data.value);
-      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${data.value}&key=${api_key}`)
+  const onSearch = (location) => {
+    fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${api_key}`
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -70,52 +97,47 @@ function MapPage(props) {
       .catch((error) => {
         console.error('Error:', error);
       });
+  };
 
-      
-    }
+  const onSearchCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
 
   const MyMapComponent = compose(
     withProps({
-      googleMapURL:
-        `https://maps.googleapis.com/maps/api/js?key=${api_key}`,
+      googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${api_key}`,
       loadingElement: <div style={{ height: `100%` }} />,
       containerElement: <div style={{ height: `80vh` }} />,
-      mapElement: <div style={{ height: `100%` }} />
+      mapElement: <div style={{ height: `100%` }} />,
     }),
     withScriptjs,
     withGoogleMap
-  )(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: latitude, lng: longitude}}>
-      {props.isMarkerShown && (
-        <Marker position={{ lat: latitude, lng: longitude }} />
-      )}
+  )((props) => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: latitude, lng: longitude }}>
+      {props.isMarkerShown && <Marker position={{ lat: latitude, lng: longitude }} />}
     </GoogleMap>
   ));
-    
-    // const [isLoading,setIsLoading] = useState(true);
-    // if (isLoading) return(
-    //     <SpinnerOfDoom/>
-    // )
-    return (
-        
-        <Container direction="column"
-            justifyContent="flex-start"
-            alignItems="center"
-            spacing={2}
-            sx={{ mx: 2, mt: '90px' }}
-            >
-            {/* <div style={{ height: '100vh', width: '100%' }} > */}
-                <TextField id="outlined-basic" label="Search" variant="outlined" />
-                <Button variant="contained" color="primary" onClick={SearchLocation}>
-                    Search
-                </Button>
-                
-                {/* <Button variant="contained" color="primary" onClick={ResetLocation}>
-                    Back to Trips
-                </Button> */}
-                <MyMapComponent isMarkerShown/>
-        </Container>
-    );
+
+  return (
+    <Container
+      direction="column"
+      justifyContent="flex-start"
+      alignItems="center"
+      spacing={2}
+      sx={{ mx: 2, mt: '90px' }}
+    >
+      <SearchBar onSearch={onSearch} onSearchCurrentLocation={onSearchCurrentLocation} />
+      <MyMapComponent isMarkerShown />
+    </Container>
+  );
 }
 
 export default MapPage;
