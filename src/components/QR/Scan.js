56b0cf8242862a7b3ee0c5c8d6b6824c08edb46c
M0ki1 <mamunoz11@miuandes.cl>
2023-10-03 00:10:16 -0300

    Finalmente QR

--- src/components/QR/Scan.js
+++ src/components/QR/Scan.js
@@ -1,77 +1,94 @@
-import {useState} from "react";
-import {QrReader} from 'react-qr-reader'
-import { Container } from '@mui/material'
+import { useState, useEffect } from "react";
+import { QrReader } from 'react-qr-reader';
+import { Container } from '@mui/material';
 import { BACK_URL } from '../../constants';
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-
-const Scan = () =>{
+const Scan = () => {
     const AUTH = localStorage.getItem('token');
-    const [data,setData] = useState('No result')
+    const [data, setData] = useState('No result');
     const apiUrl = BACK_URL + '/api/v1/add_friend';
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const position = await getCurrentPositionAsync();
+                const { latitude, longitude } = position.coords;
 
-    return(
-        <Container 
-    direction="column"
-    justifyContent="flex-start"
-    alignItems="center"
-    spacing={2}
-    sx={{ mx: 2, mt: '90px' }}
-    >
-    <QrReader onResult={(result,error)=>{
-        if(!!result){
-            setData(result?.text);
-            const queryParams = new URLSearchParams(result.text)
+                console.log('Latitude:', latitude);
+                console.log('Longitude:', longitude);
 
-         
-            const user_id = queryParams.get('user_id')
-            const friendshipToken = queryParams.get('friendship_token')
+                // Make the fetch request with latitude and longitude
+                const queryParams = new URLSearchParams(data);
+                const user_id = queryParams.get('user_id');
+                const friendshipToken = queryParams.get('friendship_token');
 
-            const to_send = {'token':friendshipToken,'id':user_id}
-            const jsonData = JSON.stringify(to_send)
+                const to_send = {
+                    'token': friendshipToken,
+                    'id': user_id,
+                    'coords': { 'latitude': latitude, 'longitude': longitude }
+                };
+                const jsonData = JSON.stringify(to_send);
 
-            fetch(apiUrl,{
-                headers: new Headers({Authorization: `Bearer ${AUTH}`,}),
-            
-            method:'POST',
-            body:jsonData
-               
-            })
-            .then((response)=>{ response.json()})
-            .then((datos)=>{
-                if(datos.error){
-                    alert(datos.error)
+                const response = await fetch(apiUrl, {
+                    headers: new Headers({ 'Authorization': `Bearer ${AUTH}` }),
+                    method: 'POST',
+                    body: jsonData
+                });
+
+                const datos = await response.json();
+
+                if (datos.error) {
+                    alert(datos.error);
+                } else {
+                    alert('Creada con éxito!');
+                    navigate('/');
                 }
-                else{
-                    alert('Creada con exito!')
-                    navigate('/')
+            } catch (error) {
+                console.error('Error:', error);
+                alert('Error, NO SE PUDO CREAR LA RELACIÓN');
+            }
+        };
+
+        const getCurrentPositionAsync = () => {
+            return new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        resolve(position);
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                );
+            });
+        };
+
+        fetchData();
+    }, [data, apiUrl, AUTH, navigate]);
+
+    return (
+        <Container
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+            spacing={2}
+            sx={{ mx: 2, mt: '90px' }}
+        >
+            <QrReader onResult={(result, error) => {
+                if (!!result) {
+                    setData(result?.text);
                 }
-                
-                // alert(datos)
 
+                if (!!error) {
+                    // Handle error if needed
+                }
+            }}
+                style={{ width: '100%' }}
+            />
 
-            })
-            .catch((error)=>{
-                alert(error)
-                console.error('Error, NO SE PUDO CREAR LA RELACION')
-            })
-            
-        }
-
-        if(!!error){
-            // console.info(error);
-        }
-    }}
-    style = {{width:'100%'}}
-    />
-
-    <p>{data}</p>
-
-
-
-    </Container>
-    )
+        </Container>
+    );
 }
-export default Scan
+
+export default Scan;
